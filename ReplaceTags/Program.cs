d40f7f3ae3fc0cf924650b4a79dd0c697ae8e19e
PavelContraMundum <pavel.sticka@gmail.com>
2024-07-07 14:44:05 +0200

    úvodní commit, nastavena výměna prvních tagů

---
+++ ReplaceTags/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFilePath = @"D:\Downloads\GenModified.txt";
+        string outputFilePath = @"D:\Downloads\GenModifiedFormatted.txt";
+
+        try
+        {
+            string content = File.ReadAllText(inputFilePath);
+
+           
+            content = Regex.Replace(content, "<italic>", "\\it");
+            content = Regex.Replace(content, "</italic>", "\\it*");
+            content = Regex.Replace(content, "<p/>", "\\p");
+            content = Regex.Replace(content, "<kap/>", "\\cl_");
+            content = Regex.Replace(content, @"<vers n=""(\d+)""/>", @"\v_$1_");
+
+            File.WriteAllText(outputFilePath, content);
+
+            Console.WriteLine($"Úpravy byly úspěšně provedeny. Výstup byl uložen do: {outputFilePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Došlo k chybě: {ex.Message}");
+        }
+    }
+}
